47f1db7175696588478707d9c8ce42415d49ae2f
marudedameo2019 <marudedameo2019@users.noreply.github.com>
2025-09-08 08:53:18 +0900

    ちょっと分かりやすくした

--- hoge.c
+++ hoge.c
@@ -28,6 +28,39 @@ static int64_t get_time_ns() {
   return (int64_t)ts.tv_sec * 1000000000 + ts.tv_nsec;
 }
 
+#define _STRINGIFY(N) #N
+#define STRINGIFY(N) _STRINGIFY(N)
+
+static const char* get_compiler_str() {
+#if defined(__clang__)
+  static const char compiler_str[] = {"Clang " STRINGIFY(__clang_major__) "." STRINGIFY(__clang_minor__) "." STRINGIFY(__clang_patchlevel__)};
+#elif defined(__GNUC__)
+  static const char compiler_str[] = {"GCC " STRINGIFY(__GNUC__) "." STRINGIFY(__GNUC_MINOR__)};
+#elif defined(_MSC_VER)
+  static const char compiler_str[] = {"VC++ " STRINGIFY(_MSC_VER)};
+#else
+  static const char compiler_str[] = {"unknown,"};
+#endif
+  return compiler_str;
+}
+
+static const char* get_arch_str() {
+#if defined(__x86_64__) || defined(_M_X64)
+  static const char arch_str[] = {"x86-64"};
+#elif defined(__i386__) || defined(_M_IX86)
+  static const char arch_str[] = {"x86"};
+#elif defined(__aarch64__) || defined(_M_ARM64)
+  static const char arch_str[] = {"ARM64"};
+#elif defined(__arm__) || defined(_M_ARM)
+  static const char arch_str[] = {"ARM"};
+#elif defined(__riscv)
+  static const char arch_str[] = {"RISC-V"};
+#else
+  static const char arch_str[] = {"unknown");
+#endif
+  return arch_str;
+}
+
 int main() {
   int r = 0;
   int count = 0;
@@ -39,28 +72,7 @@ int main() {
     total += e - s;
     ++count;
   }
-#if defined(__clang__)
-  printf("Clang %d.%d.%d,", __clang_major__, __clang_minor__, __clang_patchlevel__);
-#elif defined(__GNUC__)
-  printf("GCC %d.%d,", __GNUC__, __GNUC_MINOR__);
-#elif defined(_MSC_VER)
-  printf("VC++ %d,", _MSC_VER);
-#else
-  printf("unknown,");
-#endif
-#if defined(__x86_64__) || defined(_M_X64)
-  printf("x86-64,");
-#elif defined(__i386__) || defined(_M_IX86)
-  printf("x86,");
-#elif defined(__aarch64__) || defined(_M_ARM64)
-  printf("ARM64,");
-#elif defined(__arm__) || defined(_M_ARM)
-  printf("ARM,");
-#elif defined(__riscv)
-  printf("RISC-V,");
-#else
-  printf("unknown,");
-#endif
+  printf("%s,%s,", get_compiler_str(), get_arch_str());
   fflush(stdout);
   system("lscpu --extended=MODELNAME | awk -F: 'NR==2 {print $1; exit}' | tr -d '\\n'");
 #if defined(FASTER_CODE)
