067a777c8cb53a2f883a6caf537b01e09ab27a1e
marudedameo2019 <marudedameo2019@users.noreply.github.com>
2025-09-10 04:24:09 +0900

    出力ファイルの変更とディスアセンブル出力追加

--- hoge.sh
+++ hoge.sh
@@ -2,8 +2,15 @@ FILE="hoge.csv"
 echo "compiler,arch,processor,faster,count,mean" | tee $FILE
 for compiler in gcc clang; do
   for faster in "-DFASTER_CODE" "-DDUMMY"; do
-    "$compiler" "$faster" -O3 hoge.c -o hoge
-    ./hoge
+    if [ "$faster" = "-DFASTER_CODE" ]; then
+      faster_str=faster
+    else
+      faster_str=original
+    fi
+    exe="hoge_${compiler}_${faster_str}"
+    "$compiler" "$faster" -g -O3 hoge.c -o "$exe"
+    objdump --visualize-jumps -SDC "$exe" > "${exe}.S"
+    "./${exe}"
   done
 done | tee -a $FILE
 
