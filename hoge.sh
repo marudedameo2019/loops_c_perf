0cc1d69f9202dde0cc3765ca06e6b90b85f96e82
marudedameo2019 <marudedameo2019@users.noreply.github.com>
2025-09-29 15:26:04 +0900

    xcrunは本当にハードリンクなのか確認

--- hoge.sh
+++ hoge.sh
@@ -22,6 +22,7 @@ is_real() {
   if command -v "$compiler" >/dev/null 2>&1; then
     target_path=$(readlink -f "$target_path")
     file "$target_path"
+    ls -lAFi $(command -v "$compiler")
     ls -lAFi "$(command -v xcrun)"
     ls -lAFi "$target_path"
   else
